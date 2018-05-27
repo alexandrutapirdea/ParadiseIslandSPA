f682ed4ba5be3c4a4132c23a3b35a8d22d02fdf0
John <john_giumanca@yahoo.com>
2018-05-27 16:58:47 +0300

    john's

--- src/com/ParadiseIslandSPA/BazaDeDate.java
+++ src/com/ParadiseIslandSPA/BazaDeDate.java
@@ -1,26 +1,67 @@
 package com.ParadiseIslandSPA;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+class BazaDeDate {
 
-class BazaDeDate{
-public static void main(String args[]){
-	System.out.println("Da");
-try{
-Class.forName("com.mysql.jdbc.Driver");
+	private static String connectionLink = "jdbc:mysql://sql2.freemysqlhosting.net:3306/sql2240093\",\"sql2240093\",\"cC6!xF7*";
 
-Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/spa_database","root","");
-//here sonoo is the database name, root is the username and root is the password
-Statement stmt=con.createStatement();
+	private BazaDeDate() {
+	}
 
-ResultSet rs=stmt.executeQuery("select * from clienti");
+	public static DetaliiProdus getNumeProdusById(String idProdus) {
+		
+		PreparedStatement ps ;
+		ResultSet rs ;
+		DetaliiProdus produs;
+		String nume,tip;
+		double pret;
+		
+		try(Connection connection = DriverManager.getConnection(connectionLink);
+			ps = connection.prepareStatement("SELECT * FROM produse WHERE id_produs=?")){
+			 	ps.setString(1, idProdus);
+			 	try(rs = ps.executeQuery()){
+			 		
+			 		rs.next();
+		 			nume = rs.getString(2);
+		 			pret = rs.getDouble(3);
+		 			tip = rs.getString(4);
+			 		produs = new DetaliiProdus(nume,tip,pret);
+			 		return produs;
+			 		
+			 	} catch (SQLException ex) {
+			             Logger.getLogger(BazaDeDate.class.getName()).log(Level.SEVERE, null, ex);
+			        }
+			 	} catch (SQLException ex)  {
+		             Logger.getLogger(BazaDeDate.class.getName()).log(Level.SEVERE, null, ex);
+		        }
+		
+		
+	}
 
-while(rs.next())
-System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+	public static void main(String args[]) {
+		System.out.println("Da");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
 
-con.close();
+			Connection con = DriverManager.getConnection("jdbc:mysql://sql2.freemysqlhosting.net:3306/sql2240093",
+					"sql2240093", "cC6!xF7*");
+			// here sonoo is the database name, root is the username and root is the
+			// password
+			Statement stmt = con.createStatement();
 
-}catch(Exception e){ System.out.println(e);}
+			ResultSet rs = stmt.executeQuery("select * from clienti");
 
-}
+			while (rs.next())
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+
+			con.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
 }
