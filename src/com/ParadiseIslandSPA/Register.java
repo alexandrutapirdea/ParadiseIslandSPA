6864b5dda7773038450b447ce6401e34e74c0c05
alexandrutapirdea <tapirdea.alexandru@yahoo.com>
2018-05-28 14:36:06 +0300

    Added Servlets , JSP and CSS files for the view part of the software

---
+++ src/com/ParadiseIslandSPA/Register.java
@@ -0,0 +1,50 @@
+package com.ParadiseIslandSPA;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Register
+ */
+@WebServlet("/Register")
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		//request.getRequestDispatcher("Register.jsp").forward(request,response);
+		
+		String uri = request.getRequestURI();
+		String pageName = uri.substring(uri.lastIndexOf("/")+1);
+		if(pageName == "Register")
+			request.getRequestDispatcher("Login.jsp").forward(request,response);
+		else
+			request.getRequestDispatcher("Register.jsp").forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		//request.getRequestDispatcher("Register.jsp").forward(request,response);
+	}
+
+}
