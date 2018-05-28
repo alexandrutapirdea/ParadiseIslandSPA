88031624185c2df79890a0d8a00a957110ba8bd8
John <john_giumanca@yahoo.com>
2018-05-28 14:49:22 +0300

    john's Update to BazaDeDate

---
+++ src/com/ParadiseIslandSPA/TestBazaDeDate.java
@@ -0,0 +1,26 @@
+package com.ParadiseIslandSPA;
+
+public class TestBazaDeDate {
+
+	public static void main(String[] args) {
+		
+		DetaliiProdus produs = BazaDeDate.getProdusById("5");
+		
+		System.out.println(produs.getNumeProdus());
+		System.out.println(produs.getPretProdus());
+		System.out.println(produs.getTipProdus());
+		
+//		Client client = new Client("Zaraza", "Carlos", "carlitozzz@corunia.com");
+//		
+//		if(BazaDeDate.addNewClient(client, "123456")) {
+//			System.out.println("Succes");
+//		}
+		
+		if(BazaDeDate.loginValidation("carlitozzz@corunia.com", "123456")) {
+			System.out.println("Valid login info");
+		}else {
+			System.out.println("NOT Valid login info");
+		}
+		
+	}
+}
